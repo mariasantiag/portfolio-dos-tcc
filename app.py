1102cef297bf771e06a0fd3d7c68468c3d0b6e75
Gabriela <severogabriela4@gmail.com>
2025-10-06 08:27:21 -0300

    Corrigindo estrutura base de importação

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request, redirect
 import datetime
 import mysql.connector
+from models.control_tcc import Tcc
+from models.control_curso_orientador import Curso_orientador
+from models.control_destaques import Destaques
+from models.control_recentes import Recentes
 from models.control_usuario_admin import Usuario
 
 from flask import session
