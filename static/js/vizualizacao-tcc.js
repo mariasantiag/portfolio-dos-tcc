eb238ac12e35f01dfef6533b2505591c363ebd25
DavidArrudaSanches <david.sanches2750@gmail.com>
2025-10-07 17:10:30 -0300

    modificando codigo do js e do html ainda não esta pronto

--- static/js/vizualizacao-tcc.js
+++ static/js/vizualizacao-tcc.js
@@ -1,3 +1,4 @@
+// Codigo para proteger o site
 document.addEventListener('contextmenu', function(e){
     e.preventDefault();
     alert('Esta função foi desabilitada para proteger o conteudo do site.')
@@ -13,17 +14,19 @@ document.addEventListener('keydown', function(e){
         e.preventDefault()
         alert('Capturas de tela foram dasabilitadas')       
     }
-}, false);
+}, false); //
 
-const pdfUrl = './Proteção de PDF.pdf'
+
+// Codigo para exibir o TCC
+const pdfUrl = '../pdf/Proteção de PDF.pdf'
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = `https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js`;
 
 const loadingTask = pdfjsLib.getDocument(pdfUrl)
-const pdfViewer = document.getElementById('pdf-renderer')
+const pdfViewer = document.getElementById('tcc-renderer')
 
 loadingTask.promise.then(function(pdf){
-    console.log('PDF carregado com sucesso!')
+    console.log('TCC carregado com sucesso!')
 
     for(let pageNum = 1; pageNum<=pdf.numPages; pageNum++){
         pdf.getPage(pageNum).then(function(page){
@@ -49,32 +52,6 @@ loadingTask.promise.then(function(pdf){
 function(reason){
     console.error('Erro ao carregar o PDF', reason)
 }
-)
+) // 
 
-function viewpdf(buttonclose,elementoPDF){
-    const button = document.getElementById("btn-view-pdf");
 
-    button.addEventListener("click", () => {
-        elementoPDF.style.display = 'block';
-        buttonclose.style.display = 'block'
-        tccContainer.classList.add('is-viewing')
-    });
-    
-}
-
-function closepdf(buttonclose,elementoPDF){
-    buttonclose.addEventListener("click", () => {
-        elementoPDF.style.display = 'none'
-        buttonclose.style.display = 'none'
-        tccContainer.classList.remove('is-viewing');
-    })
-    
-}
-
-const pdfFrame = document.getElementById("pdf-viewer-container");
-const btnclose = document.getElementById("btn_close")
-const tccContainer = document.querySelector('.tcc_view');
-
-closepdf(btnclose,pdfFrame)
-
-viewpdf(btnclose,pdfFrame);
