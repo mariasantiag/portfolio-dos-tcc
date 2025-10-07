11d4719d22cf60353ca49e8695f5194588081d35
Gabriela <severogabriela4@gmail.com>
2025-10-07 10:23:05 -0300

    Corrigindo bcd

--- migrations/db_schema.sql
+++ migrations/db_schema.sql
@@ -1,75 +1,45 @@
-create database dbTcc;
-use dbTcc;
 
+DROP DATABASE IF EXISTS dbTcc;
+CREATE DATABASE dbTcc;
+USE dbTcc;
 
-create table tbOrientador (
-	nome_orientador VARCHAR(80) not null,
+-- TABELA DE CURSOS
+CREATE TABLE tbCurso (
+    cod_curso INT AUTO_INCREMENT PRIMARY KEY,
+    nome_curso VARCHAR(80) NOT NULL
+);
+
+-- TABELA DE ORIENTADORES (ligada ao curso)
+CREATE TABLE tbOrientador (
+    cod_orientador INT AUTO_INCREMENT PRIMARY KEY,
+    nome_orientador VARCHAR(80) NOT NULL,
     cod_curso INT,
-    cod_orientador INT auto_increment primary key
+    FOREIGN KEY (cod_curso) REFERENCES tbCurso(cod_curso)
 );
 
-
-create table tbCurso (
-	nome_curso VARCHAR(80) not null,
-    cod_curso INT auto_increment primary key,
-    cod_orientador INT,
-    FOREIGN KEY (cod_orientador) REFERENCES tbOrientador(cod_orientador)
-);
-
-
-
-
-
-create table tbTcc (
-	titulo VARCHAR(200) NOT NULL,
+-- TABELA DE TCCs (ligada a curso e orientador)
+CREATE TABLE tbTcc (
+    codigo INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
     autor VARCHAR(2000) NOT NULL,
     cod_orientador INT,
+    cod_curso INT,
     descricao VARCHAR(1200) NOT NULL,
-    data VARCHAR(8),
-    palavrachave1 varchar (20),
-    palavrachave2 varchar (20),
-    palavrachave3 varchar (20),
-    destaque varchar (3),
+    data VARCHAR(10),
+    palavrachave1 VARCHAR(20),
+    palavrachave2 VARCHAR(20),
+    palavrachave3 VARCHAR(20),
+    destaque VARCHAR(3),
     pdf_nome VARCHAR(255),
-    codigo INT auto_increment primary key,
-    cod_curso int,
     FOREIGN KEY (cod_curso) REFERENCES tbCurso(cod_curso),
     FOREIGN KEY (cod_orientador) REFERENCES tbOrientador(cod_orientador)
 );
 
-
-create table tbAdmin (
-	login VARCHAR(80) NOT NULL,
-    senha INT NOT NULL,
+-- TABELA DE ADMINS
+CREATE TABLE tbAdmin (
+    login VARCHAR(80) NOT NULL,
+    senha VARCHAR(100) NOT NULL,
     nome VARCHAR(80) NOT NULL,
-    codigo INT primary key
+    codigo INT PRIMARY KEY AUTO_INCREMENT
 );
 
-SELECT 
-    tcc.titulo,
-    tcc.autor,
-    tcc.descricao,
-    tcc.data,
-    tcc.palavrachave1,
-    tcc.palavrachave2,
-    tcc.palavrachave3,
-    tcc.codigo AS tcc_codigo,
-    orientador.nome_orientador ,
-    curso.nome_curso
-FROM 
-    tbTcc tcc
-INNER JOIN 
-    tbOrientador orientador ON tcc.cod_orientador = orientador.cod_orientador
-INNER JOIN 
-    tbCurso curso ON tcc.cod_curso = curso.cod_curso;
-
-
-SELECT 
-    curso.nome_curso,
-    orientador.nome_orientador
-FROM 
-    tbCurso curso
-INNER JOIN 
-    tbOrientador orientador ON curso.cod_orientador = orientador.cod_orientador;
-    
-    
