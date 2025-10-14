4a4c8a814c41c75e57eb4514e700b0bd9334e55f
Fernanda Santiago <fernandamaria5501@gmail.com>
2025-10-14 17:07:56 -0300

    destaques

--- app.py
+++ app.py
@@ -22,8 +22,10 @@ def paginaprincipal():
     
     tccs = Tcc.exibi_tcc()
     rec_tccs = Tcc.recuperar_tcc()
+    # Atualize a chamada para a nova função
+    destaques = Destaques.buscar_todos_destaques()
 
-    return render_template("principal.html", tccs = tccs, rec_tccs=rec_tccs)
+    return render_template("principal.html", tccs=tccs, rec_tccs=rec_tccs, destaques=destaques)
 
 @app.route("/paginalogin")
 def paginalogin():
--- models/control_destaques.py
+++ models/control_destaques.py
@@ -1,41 +1,42 @@
 from data.conexao import Conexao 
 
 class Destaques:
-    def ultima_mensagem():
-            #Criar conexão 
-            conexao = Conexao.criar_conexao()
+    # O nome da função foi alterado para ser mais descritivo
+    def buscar_todos_destaques():
+        conexao = Conexao.criar_conexao()
+        cursor = conexao.cursor(dictionary=True)
 
-            # O cursor será responsável por manipular
-            cursor = conexao.cursor(dictionary = True)
+        # ADICIONADO: tcc.pdf_nome para funcionar no seu HTML
+        sql = """
+            SELECT 
+                tcc.titulo,
+                tcc.autor,
+                tcc.descricao,
+                tcc.data,
+                tcc.pdf_nome, 
+                tcc.palavrachave1,
+                tcc.palavrachave2,
+                tcc.palavrachave3,
+                tcc.codigo AS tcc_codigo,
+                orientador.nome_orientador,
+                curso.nome_curso
+            FROM 
+                tbTcc AS tcc
+            INNER JOIN 
+                tbCurso AS curso ON tcc.cod_curso = curso.cod_curso
+            INNER JOIN 
+                tbTcc_Orientador AS tcc_orientador ON tcc.codigo = tcc_orientador.cod_tcc
+            INNER JOIN 
+                tbOrientador AS orientador ON tcc_orientador.cod_orientador = orientador.cod_orientador
+            WHERE 
+                tcc.destaque = 'sim';
+        """
+        cursor.execute(sql)
 
-            # Criando o sql que será executado
-            sql = """  SELECT 
-                            tcc.titulo,
-                            tcc.autor,
-                            tcc.descricao,
-                            tcc.data,
-                            tcc.palavrachave1,
-                            tcc.palavrachave2,
-                            tcc.palavrachave3,
-                            tcc.codigo AS tcc_codigo,
-                            orientador.nome AS orientador_nome,
-                            curso.nome AS curso_nome
-                        FROM 
-                            tbTcc tcc
-                        INNER JOIN 
-                            tbOrientador orientador ON tcc.cod_orientador = orientador.cod_orientador
-                        INNER JOIN 
-                            tbCurso curso ON tcc.cod_curso = curso.cod_curso
-                        WHERE 
-                            tcc.destaque = 'sim';"""
+        # ALTERADO: de fetchone() para fetchall()
+        # fetchall() retorna uma lista de todos os resultados.
+        # Se não houver resultados, retorna uma lista vazia [], o que não quebra o loop.
+        resultados = cursor.fetchall()
 
-            #Executando o comando sql
-            cursor.execute(sql)        
-
-            #Recuperando os dados e jogando em uma varialvel 
-            resultado = cursor.fetchone()
-
-            #Fecho a conexão (como não ouve alteração não preciso do commit)
-            conexao.close()
-
-            return resultado
+        conexao.close()
+        return resultados
