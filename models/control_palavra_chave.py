60f0db7d9b5e6e6a998bf27ca040ebc87dcd620c
Fernanda Santiago <fernandamaria5501@gmail.com>
2025-10-21 10:47:24 -0300

    pesquisa por autor

--- models/control_palavra_chave.py
+++ models/control_palavra_chave.py
@@ -24,13 +24,13 @@ class Palavra:
                             tbCurso ON tbTcc.cod_curso = tbCurso.cod_curso
                         inner join 
                             tborientador as orientador on tbcurso.cod_curso = orientador.cod_curso
-					  WHERE palavrachave1 LIKE %s OR palavrachave2 LIKE %s OR palavrachave3 LIKE %s OR titulo LIKE %s;
+					  WHERE palavrachave1 LIKE %s OR palavrachave2 LIKE %s OR palavrachave3 LIKE %s OR titulo LIKE %s OR autor LIKE %s;
 
         """
 
         
         palavra = f"%{palavra_chave}%"  
-        cursor.execute(sql, (palavra, palavra, palavra, palavra))
+        cursor.execute(sql, (palavra, palavra, palavra, palavra, palavra))
 
         # Retorna todos os resultados
         resultados = cursor.fetchall()
