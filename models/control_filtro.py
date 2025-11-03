467159b5bf8d871f209454fe142ebf329ac69372
Fernanda Santiago <fernandamaria5501@gmail.com>
2025-11-03 14:49:28 -0300

    resolvido: duplicação de tcc

--- models/control_filtro.py
+++ models/control_filtro.py
@@ -9,16 +9,33 @@ class Ano:
 
             # SQL para buscar os TCCs e ordenar por data (assumindo que "data" é uma coluna do tipo DATE)
             sql_tccs = """
-            SELECT 
-                tbTcc.*,
-                tbOrientador.nome_orientador,
-                tbCurso.nome_curso
-            FROM tbTcc
-            INNER JOIN tbCurso 
-                ON tbTcc.cod_curso = tbCurso.cod_curso
-            INNER JOIN tbOrientador 
-                ON tbOrientador.cod_curso = tbCurso.cod_curso
-            ORDER BY tbTcc.data ASC;
+           SELECT 
+                tcc.codigo,
+                tcc.titulo,
+                tcc.autor,
+                tcc.descricao,
+                tcc.data,
+                tcc.pdf_nome,
+                curso.nome_curso,
+                -- Agrupa os nomes dos orientadores selecionados para este TCC
+                GROUP_CONCAT(orientador.nome_orientador SEPARATOR ', ') AS nome_orientadores
+            FROM 
+                tbTcc AS tcc
+            INNER JOIN 
+                tbCurso AS curso ON tcc.cod_curso = curso.cod_curso
+            -- Faz o JOIN com a tabela de relacionamento TCC-Orientador
+            INNER JOIN 
+                tbTcc_Orientador AS tccorientador ON tcc.codigo = tccorientador.cod_tcc
+            -- Pega o nome do orientador
+            INNER JOIN
+                tbOrientador AS orientador ON tccorientador.cod_orientador = orientador.cod_orientador
+            -- Agrupa todas as linhas que pertencem ao mesmo TCC em uma só
+            GROUP BY 
+                tcc.codigo
+            -- Ordena pela data do TCC (ASC para mais antigos primeiro, como você pediu)
+            ORDER BY 
+                tcc.data ASC;
+                
             """  # DESC para obter os mais recentes primeiro
 
             cursor.execute(sql_tccs)
