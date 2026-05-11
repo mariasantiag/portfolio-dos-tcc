b5590621bdfcc3c9d45c88531da1fd9754a0ba8d
DavidArrudaSanches <david.sanches2750@gmail.com>
2026-05-11 12:10:34 -0300

    att bd

--- data/conexao.py
+++ data/conexao.py
@@ -2,23 +2,23 @@ import mysql.connector
 
 class Conexao:
     
-    def criar_conexao():
-        # Criando a conexão com o banco de dados
-        conexao = mysql.connector.connect(host = "localhost",
-                                        port = 3306,
-                                        user = "root",
-                                        password = "root",
-                                        database = "dbTcc")
-
-        return conexao
-
     # def criar_conexao():
-    #     conexao = mysql.connector.connect(
-    #                     host = "bibliotccabd.mysql.database.azure.com",
-    #                     user = "BiblioTcca_admin",
-    #                     password = "@admin-SENAI-!2025",
-    #                     database = "dbTcc"
-    #     )
+    #     # Criando a conexão com o banco de dados
+    #     conexao = mysql.connector.connect(host = "localhost",
+    #                                     port = 3306,
+    #                                     user = "root",
+    #                                     password = "root",
+    #                                     database = "dbTcc")
 
     #     return conexao
+
+    def criar_conexao():
+        conexao = mysql.connector.connect(
+                        host = "bibliotccabd.mysql.database.azure.com",
+                        user = "BiblioTcca_admin",
+                        password = "@admin-SENAI-!2025",
+                        database = "dbTcc"
+        )
+
+        return conexao
     
